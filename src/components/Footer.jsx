a6a53c4cfddb6ffc810b4c92c6a367ec8d7fc417
kimD0ngjun <chickentasty0112@gmail.com>
2023-07-11 22:07:27 +0900

    0_Footer 디자인 완료

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p className="text-sm text-slate-500">
+          개인정보 처리방침 | 이용 약관<br></br>All rights reserved @ Codestates
+        </p>
+      </footer>
+    </>
+  );
+}
