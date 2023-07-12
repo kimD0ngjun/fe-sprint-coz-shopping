9dd6d40d3f8e8173665824ca4bb7052c6f617c01
kimD0ngjun <chickentasty0112@gmail.com>
2023-07-12 14:31:19 +0900

    0_메인페이지 기본 틀 및 컴포넌트 단위 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainPage />
       <Footer />
     </div>
   );
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainGoods from "./MainGoods";
+import MainBookmark from "./MainBookmark";
+
+export default function MainPage() {
+  return (
+    <>
+      <div>
+        <MainGoods />
+        <MainBookmark />
+      </div>
+    </>
+  );
+}
