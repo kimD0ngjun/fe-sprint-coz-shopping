1da05ea6e607bb0b69e8030891f33094a00c7c83
kimD0ngjun <chickentasty0112@gmail.com>
2023-07-12 10:33:43 +0900

    3_텍스트 배치 조정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer>
+      <footer className="w-screen h-20 flex flex-row justify-center items-center">
         <p className="text-sm text-slate-500">
           개인정보 처리방침 | 이용 약관<br></br>All rights reserved @ Codestates
         </p>
