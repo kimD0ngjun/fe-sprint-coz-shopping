bc4c1f0a061f0f7276861f56d32a6df9ba61e1d2
kimD0ngjun <chickentasty0112@gmail.com>
2023-07-12 10:31:25 +0900

    1_(Header) 폰트 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,9 @@ export default function Header() {
       <header className="flex justify-around shadow-xl sticky top-0 w-screen h-20 items-center">
         <div className="flex items-center">
           <img src={logo} alt="logo" />
-          <span className="font-bold text-3xl ml-4">COZ shopping</span>
+          <span className="font-inter font-extrabold text-3xl ml-4 mt-1.5">
+            COZ shopping
+          </span>
         </div>
         <div>
           <img src={menu} alt="menu" onClick={toggleMenu} />
