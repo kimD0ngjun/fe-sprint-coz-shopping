b48b3c445ba64c9c5f5575237d1c52c1701d81f8
kimD0ngjun <chickentasty0112@gmail.com>
2023-07-13 13:13:23 +0900

    1_api 이미지 랜더링

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer";
 import MainPage from "./components/MainPage";
+import axios from "axios";
 
 function App() {
+  // 2. api get요청 후 받아온 데이터를 저장해두려고
+  // + 활용하기 위해 useState 사용
+  const [isData, setIsData] = useState(null);
+
+  // 3. 조회한 데이터를 활용하기 위해 getData 함수에 담았다
+  const getData = () => {
+    // 1. 서버에 요청 중(axios)
+    return (
+      axios
+        .get("http://cozshopping.codestates-seb.link/api/v1/products")
+        //어떻게 쓸까?
+        //isData에 저장
+        //console.log(res)로 확인해보면 data확인 가능
+        .then((res) => setIsData(res.data))
+        //에러가 나면?
+        .catch((err) => console.log(err))
+    );
+  };
+  // 4. getData함수를 발동시켜라! -> useEffect!
+  //수단일 뿐
+  //()=>{} 활용한 이유: getData말고 다른 것도 쓰고싶을 수도 있으니까
+  useEffect(() => {
+    //로딩창
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <MainPage />
+      <MainPage isData={isData} />
       <Footer />
     </div>
   );
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,14 +1,41 @@
 import React from "react";
-import MainGoods from "./MainGoods";
 import MainBookmark from "./MainBookmark";
 
-export default function MainPage() {
+export default function MainPage({ isData }) {
   return (
     <>
-      <div>
-        <MainGoods />
-        <MainBookmark />
-      </div>
+      <main>
+        <div className="font-inter text-2xl text-left font-semibold ">
+          상품 리스트
+        </div>
+        <ul className="flex">
+          {isData &&
+            isData.slice(0, 4).map((item, index) => (
+              <li key={index}>
+                <img
+                  src={item.brand_image_url || item.image_url}
+                  alt="goods"
+                  className="w-64 h-52 rounded-xl"
+                />
+              </li>
+            ))}
+        </ul>
+      </main>
+      <MainBookmark />
     </>
   );
 }
+
+// {/* 삼항연산자로 오류 막기
+//             src={isData ? isData[0].img_url : ''}*/}
+//             {isData ? (
+//                 <img
+//                   src={
+//                     isData[0].brand_image_url
+//                       ? isData[0].brand_image_url
+//                       : isData[0].image_url
+//                   }
+//                   alt="goods"
+//                   className="w-64 h-52"
+//                 />
+//               ) : null}
